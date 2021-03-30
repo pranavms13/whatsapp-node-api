353bbb55b3654f9f181077a9d43bd1413fbec8df
Michel França <fr.mich17@gmail.com>
2021-03-30 18:35:40 -0300

    Improvement implemented for code readability

--- api.js
+++ api.js
@@ -7,39 +7,53 @@ const shelljs = require('shelljs');
 const config = require('./config.json');
 const { Client } = require('whatsapp-web.js');
 const SESSION_FILE_PATH = './session.json';
+
 let sessionCfg;
 if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
 }
+
 process.title = "whatsapp-node-api";
-global.client = new Client({ puppeteer: { headless: true , args:['--no-sandbox','--disable-setuid-sandbox','--unhandled-rejections=strict'] }, session: sessionCfg});
+global.client = new Client({
+    puppeteer: { headless: true,
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--unhandled-rejections=strict'
+    ]},
+    session: sessionCfg
+});
+
 global.authed = false;
+
 const app = express();
 
 const port = process.env.PORT || config.port;
 //Set Request Size Limit 50 MB
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 client.on('qr', qr => {
-    fs.writeFileSync('./components/last.qr',qr);
+    fs.writeFileSync('./components/last.qr', qr);
 });
 
 
 client.on('authenticated', (session) => {
     console.log("AUTH!");
     sessionCfg = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
+
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
         if (err) {
             console.error(err);
         }
-        authed=true;
+        authed = true;
     });
-    try{
+
+    try {
         fs.unlinkSync('./components/last.qr')
-    }catch(err){}
+    } catch(err) {}
 });
 
 client.on('auth_failure', () => {
@@ -53,8 +67,8 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if(config.webhook.enabled){
-        axios.post(config.webhook.path, {msg : msg})
+    if (config.webhook.enabled) {
+        axios.post(config.webhook.path, { msg })
     }
 })
 client.initialize();
@@ -64,7 +78,7 @@ const groupRoute = require('./components/group');
 const authRoute = require('./components/auth');
 const contactRoute = require('./components/contact');
 
-app.use(function(req,res,next){
+app.use(function(req, res, next){
     console.log(req.method + ' : ' + req.path);
     next();
 });
