b8060539cdc626d52c8eccc13177f0619ef71755
monossido <monossido@lorenzobraghetto.com>
2021-11-16 16:46:26 +0100

    fix getqr if already authenticated without reading session.json

--- components/auth.js
+++ components/auth.js
@@ -17,13 +17,13 @@ router.get('/checkauth', async (req, res) => {
     })
 });
 
-router.get('/getqr', (req,res) => {
-    fs.readFile('components/last.qr', (err,last_qr) => {
-        fs.readFile('session.json', (serr, sessiondata) => {
-            if (err && sessiondata) {
-                res.write("<html><body><h2>Already Authenticated</h2></body></html>");
-                res.end();
-            } else if (!err && serr) {
+router.get('/getqr', async (req,res) => {
+    client.getState().then((data) => {
+        res.write("<html><body><h2>Already Authenticated</h2></body></html>");
+        res.end();
+    }).catch((err) => {
+        fs.readFile('components/last.qr', (err,last_qr) => {
+            if (!err && last_qr) {
                 var page = `
                     <html>
                         <body>
@@ -31,16 +31,16 @@ router.get('/getqr', (req,res) => {
                             </script>
                             <div id="qrcode"></div>
                             <script type="module">
-                            import QrCreator from "https://cdn.jsdelivr.net/npm/qr-creator/dist/qr-creator.es6.min.js";
-                            let container = document.getElementById("qrcode");
-                            QrCreator.render({
-                                text: "${last_qr}",
-                                radius: 0.5, // 0.0 to 0.5
-                                ecLevel: "H", // L, M, Q, H
-                                fill: "#536DFE", // foreground color
-                                background: null, // color or null for transparent
-                                size: 256, // in pixels
-                            }, container);
+                                import QrCreator from "https://cdn.jsdelivr.net/npm/qr-creator/dist/qr-creator.es6.min.js";
+                                let container = document.getElementById("qrcode");
+                                QrCreator.render({
+                                    text: "${last_qr}",
+                                    radius: 0.5, // 0.0 to 0.5
+                                    ecLevel: "H", // L, M, Q, H
+                                    fill: "#536DFE", // foreground color
+                                    background: null, // color or null for transparent
+                                    size: 256, // in pixels
+                                }, container);
                             </script>
                         </body>
                     </html>
@@ -48,8 +48,8 @@ router.get('/getqr', (req,res) => {
                 res.write(page)
                 res.end();
             }
-        })
-    });
+        });
+    })
 });
 
 module.exports = router;
